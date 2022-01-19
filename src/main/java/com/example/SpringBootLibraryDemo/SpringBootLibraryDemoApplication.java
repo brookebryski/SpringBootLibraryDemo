cc6dd474003ccbedf17fa4a010fd4a747fd0ea0d
BrookeBryski <brookeabryski@gmail.com>
2022-01-19 16:07:54 -0600

    finished up. wrote down more CRUD actions under main method

--- src/main/java/com/example/SpringBootLibraryDemo/SpringBootLibraryDemoApplication.java
+++ src/main/java/com/example/SpringBootLibraryDemo/SpringBootLibraryDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringBootLibraryDemoApplication implements CommandLineRunner {
 
@@ -21,7 +23,20 @@ public class SpringBootLibraryDemoApplication implements CommandLineRunner {
 	public void run(String[] args)
 	{
 		Library lib = repository.findById("fdsefr343").get();
-		System.out.printf(lib.getAuthor());
+		System.out.println(lib.getAuthor());
+		Library en = new Library();
+		en.setAisle(123);
+		en.setAuthor("Rahul");
+		en.setBook_name("Devops");
+		en.getIsbn("lkhs");
+		en.setId("lkhs123");
+		// repository.save(en);
+		List<Library> allrecords =  repository.findAll();
+		//
+		for(Library item : allrecords)
+		{
+			System.out.println(item.getBook_name());
+		}
+		repository.delete(en);
 	}
-
 }
