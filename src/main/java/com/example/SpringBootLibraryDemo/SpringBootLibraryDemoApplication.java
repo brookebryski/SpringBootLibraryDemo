f77fad8b0775d8e1a81cc1e96fad73324f000e67
BrookeBryski <brookeabryski@gmail.com>
2022-01-19 12:34:11 -0600

    initial commit

---
+++ src/main/java/com/example/SpringBootLibraryDemo/SpringBootLibraryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootLibraryDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLibraryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootLibraryDemo/SpringBootLibraryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootLibraryDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
