fc2daea46fe5386aba1123c2c975531718d3dee5
BrookeBryski <brookeabryski@gmail.com>
2022-01-21 12:07:51 -0600

    library controller and add response class

--- src/main/java/com/example/SpringBootLibraryDemo/Controller/LibraryController.java
+++ src/main/java/com/example/SpringBootLibraryDemo/Controller/LibraryController.java
@@ -2,9 +2,13 @@ package com.example.SpringBootLibraryDemo.Controller;
 
 import com.example.SpringBootLibraryDemo.Repository.LibraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class LibraryController {
@@ -13,10 +17,18 @@ public class LibraryController {
     LibraryRepository repository;
 
     @PostMapping("/addBook")
-    public void addBookImplementation(@RequestBody Library library)
+    public ResponseEntity<AddResponse> addBookImplementation(@RequestBody Library library)
     {
-        library.setId(library.getIsbn()+library.getAisle());
+        AddResponse ad = new AddResponse();
+        String id = library.getIsbn()+library.getAisle();
+        library.setId(id);
         repository.save(library);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("unique", id);
+        ad.setMsg("Success book is added");
+        ad.setId(id);
+       // return ad;
+        return new ResponseEntity<AddResponse>(ad,headers,HttpStatus.CREATED);
         // add book details into database
         // jpa repository -save
     }
